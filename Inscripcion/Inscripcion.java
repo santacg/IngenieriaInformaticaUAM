0ccac7ae2d6d6ea205e451738dc7e44ab2dd5bfe
EduardoJunoy <eduardojunoy@gmail.com>
2024-04-02 12:01:04 +0200

    Inscripcion comentado

--- Inscripcion/Inscripcion.java
+++ Inscripcion/Inscripcion.java
@@ -2,15 +2,32 @@ package Inscripcion;
 
 import Expofy.ClienteRegistrado;
 
+/**
+ * Clase Inscripcion.
+ * Esta clase representa una inscripción de un cliente a un evento o exposición.
+ * Mantiene la cantidad de entradas solicitadas por el cliente y la referencia
+ * al cliente que realiza la inscripción.
+ *
+ * @author Carlos García Santa, Joaquín Abad Díaz y Eduardo Junoy Ortega
+ */
 public class Inscripcion {
     private Integer nEntradas;
     private ClienteRegistrado cliente;
-   
+
+    /**
+     * Constructor que instancia una inscripción.
+     * 
+     * @param nEntradas Número de entradas solicitadas por el cliente.
+     * @param cliente   El cliente que realiza la inscripción.
+     */
     public Inscripcion(Integer nEntradas, ClienteRegistrado cliente) {
         this.nEntradas = nEntradas;
         this.cliente = cliente;
     }
 
+    /**
+     * Getters y setters que facilitan la gestión de una inscripción.
+     */
     public Integer getnEntradas() {
         return nEntradas;
     }
--- Inscripcion/InscripcionTest.java
+++ Inscripcion/InscripcionTest.java
@@ -5,15 +5,30 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Clase InscripcionTest.
+ * Esta clase contiene pruebas unitarias que validan la funcionalidad de los
+ * métodos de la clase {@link Inscripcion}.
+ */
 public class InscripcionTest {
     private Inscripcion inscripcion;
 
+    /**
+     * Configura el entorno de pruebas. Este método se ejecuta antes de cada prueba.
+     * Inicializa una instancia de {@link Inscripcion} con 5 entradas para ser usada
+     * en los tests.
+     */
     @BeforeEach
     public void setUp() {
         Integer nEntradas = 5;
         inscripcion = new Inscripcion(nEntradas);
     }
 
+    /**
+     * Prueba el método getnEntradas para asegurarse de que retorna el número
+     * correcto de entradas. Espera como resultado el número de entradas con el que
+     * fue inicializada la inscripción.
+     */
     @Test
     public void testGetnEntradas() {
         Integer expected = 5;
@@ -21,6 +36,11 @@ public class InscripcionTest {
         assertEquals(expected, actual);
     }
 
+    /**
+     * Prueba el método setnEntradas para verificar que actualiza correctamente el
+     * número de entradas. Cambia el número de entradas de la inscripción y verifica
+     * que el cambio se refleje correctamente.
+     */
     @Test
     public void testSetnEntradas() {
         Integer newNEntradas = 10;
