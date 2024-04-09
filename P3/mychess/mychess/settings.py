3527be8e732799da9fd9b98b136736e52216eec5
santacg <garciasantacarlos@gmail.com>
2024-04-09 23:28:37 +0200

    Deploy

--- P3/mychess/mychess/settings.py
+++ P3/mychess/mychess/settings.py
@@ -36,7 +36,7 @@ if 'DEBUG' in os.environ:
 else:
     DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['*']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
