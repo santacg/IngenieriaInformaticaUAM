40d6433c057519608dc3ceef3beae76bf14378d0
santacg <garciasantacarlos@gmail.com>
2024-06-16 22:35:14 +0200

    Mejoras sorteos comprobacion de errores

--- gui/controlador/ControladorSorteoFormulario.java
+++ gui/controlador/ControladorSorteoFormulario.java
@@ -7,6 +7,7 @@ import java.time.LocalTime;
 import javax.swing.JOptionPane;
 
 import gui.modelo.centroExposicion.*;
+import gui.modelo.expofy.Expofy;
 import gui.modelo.exposicion.EstadoExposicion;
 import gui.modelo.exposicion.Exposicion;
 import gui.modelo.exposicion.Hora;
@@ -50,26 +51,43 @@ public class ControladorSorteoFormulario {
             LocalTime horaSorteo = vista.getHoraSorteo();
 
             if (fechaSorteo == null) {
-                JOptionPane.showMessageDialog(vista, "Debes rellenar la fecha del sorteo o introducirla correctamente.", "Error",
+                JOptionPane.showMessageDialog(vista, "Debes rellenar la fecha del sorteo o introducirla correctamente.",
+                        "Error",
                         JOptionPane.ERROR_MESSAGE);
                 return;
             }
 
             Integer nEntradas = vista.getSorteoEntradas();
             if (nEntradas == null || nEntradas <= 0) {
-                JOptionPane.showMessageDialog(vista, "Debes rellenar el número de entradas o introducir un número de entradas correcto.", "Error",
-                        JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-            
-            
-            if (tipoSorteo.equals("Sorteo por día y hora") && (vista.getDiaSorteo() == null || horaSorteo == null)) {
-                JOptionPane.showMessageDialog(vista, "Debes rellenar el día y la hora del sorteo.", "Error",
+                JOptionPane.showMessageDialog(vista,
+                        "Debes rellenar el número de entradas o introducir un número de entradas correcto.", "Error",
                         JOptionPane.ERROR_MESSAGE);
                 return;
             }
 
-            if (tipoSorteo.equals("Sorteo por fechas") && (vista.getFechaInicioSorteo() == null || vista.getFechaFinSorteo() == null)) {
+            if (tipoSorteo.equals("Sorteo por día y hora") && (vista.getDiaSorteo() == null || horaSorteo == null)) {
+                JOptionPane.showMessageDialog(vista,
+                        "Debes rellenar el día y la hora del sorteo o hacerlo con el formato correcto.", "Error",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            if (tipoSorteo.equals("Sorteo por día y hora") && (horaSorteo.isBefore(centroExposicion.getHoraApertura())
+                    || horaSorteo.isAfter(centroExposicion.getHoraCierre()))) {
+                JOptionPane.showMessageDialog(vista, "La hora del sorteo debe estar entre las horas del centro.",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            if (tipoSorteo.equals("Sorteo por día y hora") && vista.getDiaSorteo().isBefore(fechaSorteo)) {
+                JOptionPane.showMessageDialog(vista, "La fecha del día de la exposición debe ser después del día de la fecha del sorteo", "Error",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            if (tipoSorteo.equals("Sorteo por fechas")
+                    && (vista.getFechaInicioSorteo() == null || vista.getFechaFinSorteo() == null)) {
                 JOptionPane.showMessageDialog(vista, "Debes rellenar la fecha de inicio y fin del sorteo.", "Error",
                         JOptionPane.ERROR_MESSAGE);
                 return;
@@ -82,14 +100,35 @@ public class ControladorSorteoFormulario {
                 return;
             }
 
+            if (tipoSorteo.equals("Sorteo por día y hora") && (vista.getDiaSorteo().isBefore(exposicion.getFechaInicio())
+                    || vista.getDiaSorteo().isAfter(exposicion.getFechaFin()))) {
+                JOptionPane.showMessageDialog(vista,
+                        "La fecha de eleccion del día debe estar comprendida entre la fecha de inicio y fin de la exposición.",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            if (tipoSorteo.equals("Sorteo por fechas") && (vista.getFechaInicioSorteo().isBefore(exposicion.getFechaInicio())
+                    || vista.getFechaFinSorteo().isAfter(exposicion.getFechaFin()))) {
+                JOptionPane.showMessageDialog(vista,
+                        "Las fechas de inicio y fin deben estar comprendidas entre las fecha de inicio y fin de la exposición.",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
             if (exposicion.getEstado() == EstadoExposicion.CANCELADA) {
-                JOptionPane.showMessageDialog(vista, "No se puede configurar un sorteo para una exposición cancelada.", "Error",
+                JOptionPane.showMessageDialog(vista, "No se puede configurar un sorteo para una exposición cancelada.",
+                        "Error",
                         JOptionPane.ERROR_MESSAGE);
                 return;
             }
 
             if (fechaSorteo.isBefore(exposicion.getFechaInicio()) || fechaSorteo.isAfter(exposicion.getFechaFin())) {
-                JOptionPane.showMessageDialog(vista, "La fecha del sorteo debe estar comprendida entre la fecha de inicio y fin de la exposición.", "Error",
+                JOptionPane.showMessageDialog(vista,
+                        "La fecha del sorteo debe estar comprendida entre la fecha de inicio y fin de la exposición.",
+                        "Error",
                         JOptionPane.ERROR_MESSAGE);
                 return;
             }
@@ -98,10 +137,12 @@ public class ControladorSorteoFormulario {
             switch (tipoSorteo) {
                 case "Sorteo por día y hora":
                     Hora hora = new Hora(fechaSorteo, horaSorteo, horaSorteo.plusMinutes(50), nEntradas);
-                    res = centroExposicion.confgiurarSorteoDiaHora(exposicion, fechaSorteo, nEntradas, vista.getDiaSorteo(), hora);
+                    res = centroExposicion.confgiurarSorteoDiaHora(exposicion, fechaSorteo, nEntradas,
+                            vista.getDiaSorteo(), hora);
                     break;
                 case "Sorteo por fechas":
-                    res = centroExposicion.confgiurarSorteoFechas(exposicion, fechaSorteo, nEntradas, vista.getFechaInicioSorteo(), vista.getFechaFinSorteo());
+                    res = centroExposicion.confgiurarSorteoFechas(exposicion, fechaSorteo, nEntradas,
+                            vista.getFechaInicioSorteo(), vista.getFechaFinSorteo());
                     break;
                 case "Sorteo por exposición":
                     res = centroExposicion.confgiurarSorteoExposicion(exposicion, fechaSorteo, nEntradas);
@@ -113,6 +154,14 @@ public class ControladorSorteoFormulario {
                         JOptionPane.ERROR_MESSAGE);
                 return;
             }
+
+            Expofy expofy = Expofy.getInstance();
+
+            expofy.enviarNotificacionesClientesPublicidad(
+                    "Sorteo añadido para la exposición " + exposicion.getNombre() + " en la fecha "
+                            + fechaSorteo.toString() + " con " + nEntradas + " entradas.");
+
+            JOptionPane.showMessageDialog(vista, "Sorteo configurado correctamente.");
 
             frame.actualizarTablaSorteos(centroExposicion);
             vista.dispose();
--- gui/vistas/SorteoFormulario.java
+++ gui/vistas/SorteoFormulario.java
@@ -53,7 +53,7 @@ public class SorteoFormulario extends JDialog {
         formularioAñadirSorteo(panelFormulario, constraints);
 
         if (tipoSorteo.equals("Sorteo por día y hora")) {
-            addCampo("Dia de la exposición:", diaSorteo = new JTextField(20), panelFormulario, constraints);
+            addCampo("Dia de la exposición (yyyy-mm-dd):", diaSorteo = new JTextField(20), panelFormulario, constraints);
             addCampo("Hora de la exposición (hh:mm):", horaSorteo = new JTextField(20), panelFormulario, constraints);
         } else if (tipoSorteo.equals("Sorteo por fechas")) {
             addCampo("Fecha inicio (yyyy-mm-dd):", fechaInicioSorteo = new JTextField(20), panelFormulario, constraints);
