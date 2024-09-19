5911f28e4bb37ff44ee7ab5f03255014479ad099
santacg <garciasantacarlos@gmail.com>
2024-09-19 19:55:18 +0200

    Data class almost done

--- P0/Datos.py
+++ P0/Datos.py
@@ -8,6 +8,29 @@ class Datos:
 
     # Constructor: procesar el fichero para asignar correctamente las variables nominalAtributos, datos y diccionarios
     def __init__(self, nombreFichero):       
+        df = pd.read_csv(nombreFichero, dtype={'Class': 'object'})
+
+        cols = df.columns
+
+        self.nominalAtributos = []
+        self.diccionarios = {}
+
+        for i in range(len(cols)):
+            if df[cols[i]].dtype == 'object':
+                self.nominalAtributos.append(True)
+            else:
+                self.nominalAtributos.append(False)
+            diccionario = {}
+
+            if self.nominalAtributos[i] is True:
+                valores = df[cols[i]].unique()
+                for j in range(len(valores)):
+                    diccionario[valores[j]] = j
+
+
+            self.diccionarios[cols[i]] = diccionario
+
         
-    # Devuelve el subconjunto de los datos cuyos índices se pasan como argumento
+    # Devuelve el subconjunto de los datos cuyos ï¿½ndices se pasan como argumento
     def extraeDatos(self,idx):
+        return 
---
+++ P0/Main.py
@@ -0,0 +1,8 @@
+from Datos import Datos
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+datos = Datos('heart.csv')
+print(datos.nominalAtributos)
+print(datos.diccionarios)
