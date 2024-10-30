722feac68bfc5746d8335d860b3d062988d45e58
santacg <garciasantacarlos@gmail.com>
2024-10-30 11:08:07 +0100

    Añadido cuaderno

--- Clasificador.py
+++ Clasificador.py
@@ -26,15 +26,18 @@ class Clasificador:
     def clasifica(self, datosTest, nominalAtributos, diccionario):
         return np.array(None)
 
+    # Obtiene el numero de aciertos y errores para calcular la tasa de fallo
     def error(self, datos, pred):
+        # Aqui se compara la prediccion (pred) con las clases reales y se calcula el error
+        # devuelve el error
         total_filas = datos.shape[0]
-        clases_reales = datos.loc[:, 'Class']
+        clases = datos.loc[:, 'Class']
 
         aciertos = 0
         errores = 0
         for i in range(total_filas):
             clase_predicha = pred[i]
-            clase_real = clases_reales.iloc[i]
+            clase_real = clases.iloc[i]
 
             if clase_predicha == clase_real:
                 aciertos += 1
@@ -44,7 +47,15 @@ class Clasificador:
         ratio_error = errores / (aciertos + errores)
         return ratio_error
 
+    # Realiza una clasificacion utilizando una estrategia de particionado determinada
     def validacion(self, particionado, dataset, clasificador, seed=None):
+        # Creamos las particiones siguiendo la estrategia llamando a particionado.creaParticiones
+        # - Para validacion cruzada: en el bucle hasta nv entrenamos el clasificador con la particion de train i
+        # y obtenemos el error en la particion de test i
+        # - Para validacion simple (hold-out): entrenamos el clasificador con la particion de train
+        # y obtenemos el error en la particion test. Otra opción es repetir la validación simple un número especificado de veces, obteniendo en cada una un error. Finalmente se calcularía la media.
+        # devuelve el vector con los errores por cada partición
+
         error = []
         particiones = particionado.creaParticiones(dataset.datos, seed)
         atributos_nominales = dataset.nominalAtributos
@@ -78,96 +89,132 @@ class Clasificador:
                 error.append(clasificador.error(
                     test, clasificaciones))
 
-            error = np.mean(error)
-
         return error
 
 
 ##############################################################################
 
-
 class ClasificadorNaiveBayes(Clasificador):
 
     def __init__(self, laplace=False):
-        self.priori = {}
-        self.verosimilitud = {}
-        self.laplace = laplace
+        self.priori = {}  # Aquí guardamos las probabilidades a priori de cada clase
+        self.verosimilitud = {}  # Aquí almacenamos las probabilidades condicionales (verosimilitud)
+        self.laplace = laplace  # Indicamos si aplicaremos la suavización de Laplace
 
-    # TODO: implementar
     def entrenamiento(self, datosTrain, nominalAtributos, diccionario):
-        total_filas = datosTrain.shape[0]
-        clases_reales = datosTrain.loc[:, 'Class']
-        clases_unicas, conteos_clases = np.unique(
-            clases_reales, return_counts=True)
+        filas = datosTrain.shape[0]
+        columnas = datosTrain.shape[1] - 1
 
-        # Calcular probabilidades a priori
+        # Obtenemos las clases únicas y calculamos sus probabilidades a priori
+        clases = datosTrain.loc[:, 'Class']
+        clases_unicas, count_clases = np.unique(clases, return_counts=True)
+
+        # Calculamos y guardamos la probabilidad a priori de cada clase
         for idx, clase in enumerate(clases_unicas):
-            self.priori[clase] = conteos_clases[idx] / total_filas
+            self.priori[clase] = count_clases[idx] / filas
 
-        # Calcular probabilidades de verosimilitud
-        total_columnas = datosTrain.shape[1] - 1
-        for i in range(total_columnas):
-            atributo_series = datosTrain.iloc[:, i]
+        # Procesamos cada columna del conjunto de entrenamiento
+        for i in range(columnas):
+            atributos = datosTrain.iloc[:, i]
+            nombre_atributo = datosTrain.columns[i]
 
-            if nominalAtributos[i] is True:
-                valores_unicos = np.unique(atributo_series)
+            # Si el atributo es categórico
+            if nominalAtributos[i]:
+                valores_unicos = np.unique(atributos)
                 num_valores_unicos = len(valores_unicos)
+                if nombre_atributo not in self.verosimilitud:
+                    self.verosimilitud[nombre_atributo] = {}
+
+                # Calculamos la verosimilitud de cada valor categórico para cada clase
                 for valor in valores_unicos:
-                    self.verosimilitud[valor] = {}
-                    for idx, clase in enumerate(clases_unicas):
-                        conteo = ((atributo_series == valor) & (
-                            clases_reales == clase)).sum()
+                    if valor not in self.verosimilitud[nombre_atributo]:
+                        self.verosimilitud[nombre_atributo][valor] = {}
+                    for idx_clase, clase in enumerate(clases_unicas):
+                        # Contamos las ocurrencias del valor dado en la clase actual
+                        count = ((atributos == valor) & (clases == clase)).sum()
 
-                        if self.laplace is True:
-                            conteo += 1
-                            denominador = conteos_clases[idx] + \
-                                num_valores_unicos
+                        # Aplicamos Laplace si está activado
+                        if self.laplace:
+                            count += 1
+                            denominador = count_clases[idx_clase] + num_valores_unicos
                         else:
-                            denominador = conteos_clases[idx]
+                            denominador = count_clases[idx_clase]
+                            if denominador == 0:  # Evitamos división por cero
+                                denominador = 1e-6  
 
-                        self.verosimilitud[valor][clase] = conteo / denominador
+                        # Guardamos la probabilidad condicional de cada valor para cada clase
+                        self.verosimilitud[nombre_atributo][valor][clase] = count / denominador
             else:
-                nombre_atributo = datosTrain.columns[i]
-                self.verosimilitud[nombre_atributo] = {}
-                for clase in clases_unicas:
-                    valores_clase = atributo_series[clases_reales == clase]
+                # Si el atributo es numérico, calculamos media y desviación estándar para cada clase
+                if nombre_atributo not in self.verosimilitud:
+                    self.verosimilitud[nombre_atributo] = {}
+                for idx_clase, clase in enumerate(clases_unicas):
+                    valores_clase = atributos[clases == clase]
                     media = np.mean(valores_clase)
                     std_dev = np.std(valores_clase)
+                    
+                    # Evitamos una desviación estándar cero que pueda causar errores
+                    if std_dev == 0:
+                        std_dev = 1e-6
+
+                    # Guardamos la media y desviación estándar para el valor de la clase
                     self.verosimilitud[nombre_atributo][clase] = {
                         "media": media, "std_dev": std_dev}
 
-        return
+        return    
 
     def clasifica(self, datosTest, nominalAtributos, diccionario):
-        total_filas = datosTest.shape[0]
-        total_columnas = datosTest.shape[1] - 1
-        probabilidades = np.empty((total_filas, len(self.priori)))
-        clases_unicas = np.unique(datosTest.loc[:, 'Class'])
+        filas = datosTest.shape[0]
+        columnas = datosTest.shape[1] - 1
+
+        probabilidades = np.empty((filas, len(self.priori)))  # Matriz para las probabilidades de cada clase
+        clases_unicas = list(self.priori.keys())  # Usamos las clases obtenidas en el entrenamiento
         clasificaciones = []
 
-        for i in range(total_filas):
+        # Iteramos sobre cada muestra en el conjunto de prueba
+        for i in range(filas):
             fila_datos = datosTest.iloc[i]
-            for idx, clase in enumerate(self.priori):
+            for idx, clase in enumerate(clases_unicas):
+                # Inicializamos la probabilidad posterior con la probabilidad a priori de la clase
                 posteriori = self.priori[clase]
-                for j in range(total_columnas):
+                for j in range(columnas):
+                    nombre_atributo = datosTest.columns[j]
                     valor = fila_datos.iloc[j]
-                    if nominalAtributos[j] is True:
-                        posteriori *= self.verosimilitud[valor][clase]
+                    
+                    # Para atributos categóricos
+                    if nominalAtributos[j]:
+                        if valor in self.verosimilitud[nombre_atributo]:
+                            prob_atributo = self.verosimilitud[nombre_atributo][valor].get(clase, 0)
+                            posteriori *= prob_atributo
+                        else:
+                            # Manejamos los valores no vistos en el entrenamiento
+                            if self.laplace:
+                                num_valores_unicos = len(self.verosimilitud[nombre_atributo]) + 1
+                                posteriori *= 1 / num_valores_unicos
+                            else:
+                                posteriori *= 0
                     else:
-                        nombre_atributo = datosTest.columns[j]
+                        # Para atributos numéricos, calculamos la probabilidad usando distribución normal
                         media = self.verosimilitud[nombre_atributo][clase]['media']
                         std_dev = self.verosimilitud[nombre_atributo][clase]['std_dev']
-                        posteriori *= st.norm.pdf(
-                            valor, loc=media, scale=std_dev)
+                        prob_atributo = st.norm.pdf(valor, loc=media, scale=std_dev)
+                        posteriori *= prob_atributo
 
+                # Guardamos la probabilidad total para la clase actual
                 probabilidades[i][idx] = posteriori
 
-            probabilidades[i] /= np.sum(probabilidades[i])
-            clasificaciones.append(
-                clases_unicas[np.argmax(probabilidades[i])])
+            # Normalizamos las probabilidades y evitamos problemas cuando la suma es cero
+            suma_probabilidades = np.sum(probabilidades[i])
+            if suma_probabilidades > 0:
+                probabilidades[i] /= suma_probabilidades
+            else:
+                # Distribuimos probabilidades uniformes si todas son cero
+                probabilidades[i] = np.full(len(clases_unicas), 1 / len(clases_unicas))
+            
+            # Asignamos la clase con mayor probabilidad
+            clasificaciones.append(clases_unicas[np.argmax(probabilidades[i])])
 
         return np.array(clasificaciones)
-
 
 class ClasificadorKNN(Clasificador):
     def __init__(self, K=3, normalize=True):
@@ -200,21 +247,10 @@ class ClasificadorKNN(Clasificador):
             self.training_data['Class'] = self.training_data['Class'].astype(
                 int)
 
-        if self.normalize:
-            # Normalizar x = (x-x_min)/(x_max-x_min)
-            for i, column in enumerate(self.training_data.columns):
-                if not nominalAtributos[i] and column != 'Class':
-                    min_val = self.training_data[column].min()
-                    max_val = self.training_data[column].max()
-                    self.min_vals[column] = min_val
-                    self.max_vals[column] = max_val
-                    self.training_data[column] = (
-                        self.training_data[column] - min_val) / (max_val - min_val)
-        else:
-            for i, column in enumerate(self.training_data.columns):
-                if not nominalAtributos[i] and column != 'Class':
-                    self.min_vals[column] = 0
-                    self.max_vals[column] = 1
+        for i, column in enumerate(self.training_data.columns):
+            if not nominalAtributos[i] and column != 'Class':
+                self.min_vals[column] = 0
+                self.max_vals[column] = 1
 
     def clasifica(self, datosTest, nominalAtributos, diccionarios):
         test_data = datosTest.copy()
@@ -230,13 +266,6 @@ class ClasificadorKNN(Clasificador):
             test_data['Class'] = test_data['Class'].map(mapping)
             test_data['Class'] = test_data['Class'].astype(int)
 
-        if self.normalize:
-            for i, column in enumerate(test_data.columns):
-                if not nominalAtributos[i] and column != 'Class':
-                    min_val = self.min_vals[column]
-                    max_val = self.max_vals[column]
-                    test_data[column] = (
-                        test_data[column] - min_val) / (max_val - min_val)
 
         training_features = self.training_data.drop(columns=['Class']).values
         test_features = test_data.drop(columns=['Class']).values
