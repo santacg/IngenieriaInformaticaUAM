569b1ea5977a3b7b5c9568c3843b70410cb4c511
Carlos García Santa <114600038+santacg@users.noreply.github.com>
2024-11-08 12:38:21 +0100

    Update p2_tarea2.py

--- P2/p2_tarea2.py
+++ P2/p2_tarea2.py
@@ -8,10 +8,12 @@
 
 # librerias y paquetes por defecto
 import numpy as np
+import skimage.color
 from p2_tests import test_p2_tarea2
 
 # Incluya aqui las librerias que necesite en su codigo
-# ...
+import skimage
+from matplotlib import pyplot as plt
 
 def descripcion_puntos_interes(imagen, coords_esquinas, vtam = 8, nbins = 16, tipoDesc='hist'):
     """
@@ -50,8 +52,18 @@ def descripcion_puntos_interes(imagen, coords_esquinas, vtam = 8, nbins = 16, ti
     descriptores = np.empty(shape=[0,0]) # iniciamos la variable de salida (numpy array)
     new_coords_esquinas = np.empty(shape=[0,0]) # iniciamos la variable de salida (numpy array)
 
-    #incluya su codigo aqui
-    #...    
+    # Pasamos la imagen a float
+    imagen = imagen.astype(np.float64)
+
+    # Normalizamos la imagen
+    imagen = imagen / np.sum(imagen)
+
+    # Calculamos los histogramas para cada punto de interes
+    for coords_esquina in coords_esquinas:
+        plt.hist(np.histogram(np.ndarray.flatten(imagen[coords_esquina]), bins=np.arange(nbins)))
+        plt.show()
+
+
 
     return descriptores, new_coords_esquinas
 
@@ -59,13 +71,13 @@ if __name__ == "__main__":
     print("Practica 2 - Tarea 2 - Test autoevaluación\n")                
 
     ## tests descriptor tipo 'hist' (tarea 2a)
-    print("Tests completados = " + str(test_p2_tarea2(disptime=-1,stop_at_error=False,debug=False,tipoDesc='hist'))) #analizar todas las imagenes y esquinas del test
+    #print("Tests completados = " + str(test_p2_tarea2(disptime=-1,stop_at_error=False,debug=False,tipoDesc='hist'))) #analizar todas las imagenes y esquinas del test
     #print("Tests completados = " + str(test_p2_tarea2(disptime=1,stop_at_error=False,debug=False,tipoDesc='hist'))) #analizar todas las imagenes y esquinas del test, mostrar imagenes con resultados (1 segundo)
-    #print("Tests completados = " + str(test_p2_tarea2(disptime=-1,stop_at_error=True,debug=True,tipoDesc='hist'))) #analizar todas las imagenes y esquinas del test, pararse en errores y mostrar datos
+    print("Tests completados = " + str(test_p2_tarea2(disptime=-1,stop_at_error=True,debug=True,tipoDesc='hist'))) #analizar todas las imagenes y esquinas del test, pararse en errores y mostrar datos
     #print("Tests completados = " + str(test_p2_tarea2(disptime=-1,stop_at_error=True,debug=True,tipoDesc='hist',imgIdx = 3, poiIdx = 7))) #analizar solamente imagen #2 y esquina #7    
 
     ## tests descriptor tipo 'mag-ori' (tarea 2b)
     #print("Tests completados = " + str(test_p2_tarea2(disptime=-1,stop_at_error=False,debug=False,tipoDesc='mag-ori'))) #analizar todas las imagenes y esquinas del test
     #print("Tests completados = " + str(test_p2_tarea2(disptime=0.1,stop_at_error=False,debug=False,tipoDesc='mag-ori'))) #analizar todas las imagenes y esquinas del test, mostrar imagenes con resultados (1 segundo)
     #print("Tests completados = " + str(test_p2_tarea2(disptime=-1,stop_at_error=True,debug=True,tipoDesc='mag-ori'))) #analizar todas las imagenes y esquinas del test, pararse en errores y mostrar datos
-    #print("Tests completados = " + str(test_p2_tarea2(disptime=1,stop_at_error=True,debug=True,tipoDesc='mag-ori',imgIdx = 3,poiIdx = 7))) #analizar solamente imagen #1 y esquina #7       
+    #print("Tests completados = " + str(test_p2_tarea2(disptime=1,stop_at_error=True,debug=True,tipoDesc='mag-ori',imgIdx = 3,poiIdx = 7))) #analizar solamente imagen #1 y esquina #7           
