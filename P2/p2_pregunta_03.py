c523acb003cf34f52aeff672e14a663bbf0a6f71
santacg <garciasantacarlos@gmail.com>
2024-11-14 23:34:55 +0100

    preguntas

--- P2/p2_pregunta_03.py
+++ P2/p2_pregunta_03.py
@@ -18,12 +18,12 @@ from p2_tests import test_p2_tarea1
 from p2_tests import test_p2_tarea2
 from p2_tests import test_p2_tarea3
 
-print("Tests completados = " + str(test_p2_tarea1(disptime=-1,stop_at_error=False,debug=False))) #analizar todos los casos sin pararse en errores
+print("Tests completados = " + str(test_p2_tarea1(disptime=0.5,stop_at_error=False,debug=False))) #analizar todos los casos sin pararse en errores
 
-print("Tests completados = " + str(test_p2_tarea2(disptime=1,stop_at_error=False,debug=False,tipoDesc='hist'))) #analizar todas las imagenes y esquinas del test, mostrar imagenes con resultados (1 segundo)
-print("Tests completados = " + str(test_p2_tarea2(disptime=1,stop_at_error=False,debug=False,tipoDesc='mag-ori'))) #analizar todas las imagenes y esquinas del test, mostrar imagenes con resultados (1 segundo)
+print("Tests completados = " + str(test_p2_tarea2(disptime=0.5,stop_at_error=False,debug=False,tipoDesc='hist'))) #analizar todas las imagenes y esquinas del test, mostrar imagenes con resultados (1 segundo)
+print("Tests completados = " + str(test_p2_tarea2(disptime=0.5,stop_at_error=False,debug=False,tipoDesc='mag-ori'))) #analizar todas las imagenes y esquinas del test, mostrar imagenes con resultados (1 segundo)
 
-print("Tests completados = " + str(test_p2_tarea3(disptime=-1,stop_at_error=False,debug=False,tipoDesc='hist',tipoCorr='mindist'))) #analizar todas las imagenes con descriptor 'hist'
-print("Tests completados = " + str(test_p2_tarea3(disptime=1,stop_at_error=False,debug=False,tipoDesc='mag-ori',tipoCorr='mindist'))) #analizar todas las imagenes con descriptor 'mag-ori'
+print("Tests completados = " + str(test_p2_tarea3(disptime=0.5,stop_at_error=False,debug=False,tipoDesc='hist',tipoCorr='mindist'))) #analizar todas las imagenes con descriptor 'hist'
+print("Tests completados = " + str(test_p2_tarea3(disptime=0.5,stop_at_error=False,debug=False,tipoDesc='mag-ori',tipoCorr='mindist'))) #analizar todas las imagenes con descriptor 'mag-ori'
 
 
