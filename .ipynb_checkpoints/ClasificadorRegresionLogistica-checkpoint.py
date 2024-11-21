844154aec5b88e081385eff52eed10135c6e6790
santacg <garciasantacarlos@gmail.com>
2024-11-21 11:41:15 +0100

    todo

--- .ipynb_checkpoints/ClasificadorRegresionLogistica-checkpoint.py
+++ .ipynb_checkpoints/ClasificadorRegresionLogistica-checkpoint.py
@@ -10,35 +10,47 @@ class ClasificadorRegresionLogistica(Clasificador):
 
 
     def entrenamiento(self, datosTrain, nominalAtributos, diccionario):
+        # Extraemos las etiquetas y los datos de entrada sin las etiquetas
         target = datosTrain['Class'].values.reshape(-1, 1)
         datosTrain = datosTrain.drop(columns='Class').values
 
         columnas = datosTrain.shape[1]
+        # Inicializamos los pesos de forma aleatoria en el rango -0.5, 0.5
         self.vector_pesos = np.random.uniform(-0.5, 0.5, size=(1, columnas))
 
+         # Entrenamos el modelo durante el número de épocas especificado
         for _ in range(self.epocas):
+            # Cálculo del producto punto entre los datos y los pesos
             z = np.dot(datosTrain, self.vector_pesos.T)
-
+            
+            # Evitamos el Overflow limitando los valores de z
             z = np.clip(z, -700, 700)
-            exp = np.exp(-z)
 
+            # Aplicamos la función sigmoide
+            exp = np.exp(-z)
             sigma = (1 / (1 + exp))
+            
             error = sigma - target
             gradiente = np.dot(datosTrain.T, error) / datosTrain.shape[0]
+            
+            # Actualizamos los pesos usando el gradiente y la tasa de aprendizaje
             self.vector_pesos -= self.aprendizaje * gradiente.T
 
         return
     
     def clasifica(self, datosTest, nominalAtributos, diccionario, return_scores=False):
         datosTest = datosTest.drop(columns='Class').values
-
+        # Calculamos el producto punto entre los datos y los pesos entrenados
         z = np.dot(datosTest, self.vector_pesos.T)
-
+        
+        # Evitamos el Overflow limitando los valores de z
         z = np.clip(z, -700, 700)
+        
+        # Aplicamos la función sigmoide
         exp = np.exp(-z)
-
         scores = (1 / (1 + exp))
 
+        # Clasificamos segun un umbral de 0.5
         clasificaciones = np.where(scores >= 0.5, 1, 0)
 
         return (scores.flatten(), clasificaciones.flatten()) if return_scores else clasificaciones.flatten()
