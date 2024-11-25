fe88034acf26bf8b9832c760811d94afc214763a
Miguel Ángel González Gallego <mangelgonzalez.2003@gmail.com>
2024-11-25 16:36:43 +0100

    subo algo

---
+++ P3/P3_bow_code/p3_tarea1.py
@@ -0,0 +1,80 @@
+# Tratamiento de Señales Visuales/Tratamiento de Señales Multimedia I @ EPS-UAM
+# Practica 3: Reconocimiento de escenas con modelos BOW/BOF
+# Tarea 1: modelo BOW/BOF
+
+# AUTOR1: GARCÍA SANTA, CARLOS
+# AUTOR2: GONZÁLEZ GALLEGO, MIGUEL ANGEL
+# PAREJA/TURNO: 02/NUMERO_TURNO
+
+# librerias y paquetes por defecto
+from p3_tests import test_p3_tarea1
+from sklearn.cluster import KMeans
+import numpy as np
+
+def construir_vocabulario(list_img_desc, vocab_size=5, max_iter=300):
+    """   
+    # Esta funcion utiliza K-Means para agrupar los descriptores en "vocab_size" clusters.
+    #
+    # Argumentos de entrada:
+    # - list_array_desc: Lista 1xN con los descriptores de cada imagen. Cada posicion de la lista 
+    #                   contiene (MxD) numpy arrays que representan UNO O VARIOS DESCRIPTORES 
+    #                   extraidos sobre la imagen
+    #                   - M es el numero de vectores de caracteristicas/features de cada imagen 
+    #                   - D el numero de dimensiones del vector de caracteristicas/feature.    
+    #   - vocab_size: int, numero de palabras para el vocabulario a construir.    
+    #   - max_iter: int, numero maximo de iteraciones del algoritmo KMeans
+    #
+    # Argumentos de salida:
+    #   - vocabulario: Numpy array de tamaño [vocab_size, D], 
+    #                   que contiene los centros de los clusters obtenidos por K-Means
+    #
+    #
+    # NOTA: se sugiere utilizar la funcion sklearn.cluster.KMeans
+    # https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html     
+    """
+    vocabulario = np.empty(shape=[vocab_size,list_img_desc[0].shape[1]]) # iniciamos la variable de salida (numpy array)
+
+    # Creamos kmeans
+    kmeans = KMeans(n_clusters=vocab_size, max_iter = max_iter, random_state=0)
+
+    # Obtenemos los centros de los clusters por K-Means
+    vocabulario = kmeans.cluster_centers_
+    
+    # Concatenamos la lista list_img_desc
+    concatenate = np.concatenate(list_img_desc)
+
+    # Ajustamos kmeans a la concatenacion de descriptores
+    kmeans.fit(concatenate)
+
+    return vocabulario
+
+def obtener_bags_of_words(list_img_desc, vocab):
+    """
+    # Esta funcion obtiene el Histograma Bag of Words para cada imagen
+    #
+    # Argumentos de entrada:
+    # - list_img_desc: Lista 1xN con los descriptores de cada imagen. Cada posicion de la lista 
+    #                   contiene (MxD) numpy arrays que representan UNO O VARIOS DESCRIPTORES 
+    #                   extraidos sobre la imagen
+    #                   - M es el numero de vectores de caracteristicas/features de cada imagen 
+    #                   - D el numero de dimensiones del vector de caracteristicas/feature.  
+    #   - vocab: Numpy array de tamaño [vocab_size, D], 
+    #                  que contiene los centros de los clusters obtenidos por K-Means.   
+    #
+    # Argumentos de salida: 
+    #   - list_img_bow: Array de Numpy [N x vocab_size], donde cada posicion contiene 
+    #                   el histograma bag-of-words construido para cada imagen.
+    #
+    """
+    # iniciamos la variable de salida (numpy array)
+    list_img_bow = np.empty(shape=[len(list_img_desc),len(vocab)]) 
+  
+    
+    
+    return list_img_bow
+
+if __name__ == "__main__":    
+    dataset_path = './datasets/scenes15/'
+    print("Practica 3 - Tarea 1 - Test autoevaluación\n")                    
+    print("Tests completados = " + str(test_p3_tarea1(dataset_path,stop_at_error=False,debug=False))) #analizar todos los casos sin pararse en errores ni mostrar datos
+    #print("Tests completados = " + str(test_p3_tarea1(dataset_path,stop_at_error=True,debug=True))) #analizar todos los casos, pararse en errores y mostrar datos
---
+++ P3/P3_bow_code/p3_tarea2.py
@@ -0,0 +1,86 @@
+# Tratamiento de Señales Visuales/Tratamiento de Señales Multimedia I @ EPS-UAM
+# Practica 3: Reconocimiento de escenas con modelos BOW/BOF
+# Tarea 2: extraccion de caracteristicas
+
+# AUTOR1: GARCÍA SANTA, CARLOS
+# AUTOR2: GONZÁLEZ GALLEGO, MIGUEL ANGEL
+# PAREJA/TURNO: 02/NUMERO_TURNO
+
+# librerias y paquetes por defecto
+from p3_tests import test_p3_tarea2
+import numpy as np
+
+# Incluya aqui las librerias que necesite en su codigo
+# ...
+
+def obtener_features_tiny(path_imagenes, tamano = 16):
+    """
+    # Esta funcion calcula un descriptor basado en submuestreo para una lista de imagenes.
+    # Para cada imagen, el descriptor se basa en convertir la imagen a gris, redimensionar 
+    # la imagen y posteriormente convertirla en un vector fila.        
+    #
+    # Argumentos de entrada:
+    #   - path_imagenes: lista, una lista de Python con N strings. Cada string corresponde
+    #                    con la ruta/path de la imagen en el sistema, que se debe cargar 
+    #                    para calcular la caracteristica Tiny.
+    #   - tamano:        int, valor de la dimension de cada imagen resultante
+    #                    tras aplicar el redimensionado de las imagenes de entrada
+    #
+    # Argumentos de salida:
+    # - list_img_desc_tiny: Lista 1xN, donde cada posicion representa los descriptores calculados para cada imagen.
+    #                       En el caso de caracteristicas Tiny, cada posicion contiene UN DESCRIPTOR 
+    #                       con dimensiones 1xD donde D el numero de dimensiones del vector de caracteristicas/feature Tiny.
+    #                       Ejemplo: si tamano=16, entonces D = 16 * 16 = 256 y el vector será 1x256
+    """       
+    # Iniciamos variable de salida
+    list_img_desc_tiny = list()
+
+    #...     
+    
+    return list_img_desc_tiny
+
+def obtener_features_hog(path_imagenes, tamano=100, orientaciones=9,pixeles_por_celda=(8, 8),celdas_bloque=(2,2)):
+    """
+    # Esta funcion calcula un descriptor basado en Histograma de Gradientes Orientados (HOG) 
+    # para una lista de imagenes. Para cada imagen, se convierte la imagen a escala de grises, redimensiona 
+    # la imagen y el descriptor se basa aplicar HOG a la imagen que posteriormente se convierte a un vector fila.      
+    #
+    # Argumentos de entrada:
+    #   - path_imagenes: lista, una lista de Python con N strings. Cada string corresponde
+    #                    con la ruta/path de la imagen en el sistema, que se debe cargar 
+    #                    para calcular la caracteristica HOG.
+    #   - tamano:        int, valor de la dimension de cada imagen resultante
+    #                    tras aplicar el redimensionado de las imagenes de entrada
+    #   - orientaciones: int, numero de orientaciones a considerar en el descriptor HOG
+    #   - pixeles_por_celda: tupla de int, numero de pixeles en cada celdas del descriptor HOG
+    #   - celdas_bloque:  tupla de int, numero de celdas a considerar en cada bloque del descriptor HOG
+    #
+    # Argumentos de salida:
+    # - list_img_desc_hog: Lista 1xN, donde cada posicion representa los descriptores calculados para cada imagen.
+    #                       En el caso de caracteristicas HOG, cada posicion contiene VARIOS DESCRIPTORES 
+    #                       con dimensiones MxD donde 
+    #                       - M es el numero de vectores de caracteristicas/features de cada imagen 
+    #                       - D el numero de dimensiones del vector de caracteristicas/feature HOG.
+    #                       Ejemplo: Para una imagen de 100x100 y con valores por defecto, 
+    #                       para cada imagen se obtienen M=81 vectores/descriptores de D=144 dimensiones.  
+    #   
+    # NOTA: para cada imagen utilice la funcion 'skimage.feature.hog' con los argumentos 
+    #                           "orientations=orientaciones, pixels_per_cell=pixeles_por_celda, 
+    #                           cells_per_block=celdas_bloque, feature_vector=False"
+    #       obtendra un array numpy de cinco dimensiones con 'shape' (S1,S2,S3,S4,S5), en este caso:
+    #                      - 'M' se corresponde a las dos primeras dimensiones S1, S2
+    #                      - 'D' se corresponde con las tres ultimas dimensiones S3,S4,S5
+    #       Con lo cual transforme su vector (S1,S2,S3,S4,S5) en (M,D). Se sugiere utilizar la funcion 'numpy.reshape'
+    """
+    # Iniciamos variable de salida
+    list_img_desc_hog = list()
+
+    #...
+
+    return list_img_desc_hog
+    
+if __name__ == "__main__":    
+    dataset_path = './datasets/scenes15/'
+    print("Practica 3 - Tarea 2 - Test autoevaluación\n")                    
+    print("Tests completados = " + str(test_p3_tarea2(dataset_path,stop_at_error=False,debug=False))) #analizar todos los casos sin pararse en errores ni mostrar datos
+    #print("Tests completados = " + str(test_p3_tarea2(dataset_path,stop_at_error=True,debug=True))) #analizar todos los casos, pararse en errores y mostrar datos
