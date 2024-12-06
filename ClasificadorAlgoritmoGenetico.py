4cf36d5a4fee9f47c8cda13aa10d251f0bf39e2f
santacg <garciasantacarlos@gmail.com>
2024-12-06 11:45:52 +0100

    Fitness mal hecho

--- ClasificadorAlgoritmoGenetico.py
+++ ClasificadorAlgoritmoGenetico.py
@@ -1,20 +1,27 @@
+import enum
 import numpy as np
+import pandas as pd
+from bitarray.util import ba2int
 from bitarray import bitarray
 from Clasificador import Clasificador
 
 class ClasificadorAlgoritmoGenetico(Clasificador):
 
-    def __init__(self, poblacion_size=100, epochs=100, max_reglas=5, elitismo=0.05):
+    def __init__(self, poblacion_size=100, epochs=100, max_reglas=5, elitismo=0.05, seed=None):
         self.poblacion_size = poblacion_size
         self.epochs = epochs
         self.max_reglas = max_reglas
         self.elitismo = elitismo
+        self.seed = seed
 
-    def entrenamiento(self, datosTrain, nominalAtributos, diccionario):
+
+    def generar_poblacion(self, datosTrain):
+        np.random.seed(self.seed)
+
         # Generamos la poblacion
         poblacion = []
 
-        # Obtenemos el número de columnas
+        # Obtenemos el número de columnas menos la clase
         cols_size = datosTrain.shape[1]
 
         valores_unicos = []
@@ -27,25 +34,60 @@ class ClasificadorAlgoritmoGenetico(Clasificador):
         # Para cada individuo obtenemos sus reglas 
         for _ in range(self.poblacion_size):
             # Establecemos aleatoriamente el número de reglas para el individuo
-            n_reglas = np.random.randint(1, self.max_reglas)
+            n_reglas = np.random.randint(1, self.max_reglas + 1)
             individuo = []
-            for n_regla in range(n_reglas):
+            for _ in range(n_reglas):
                 reglas = []
                 # Para cada atributo generamos una regla
                 for i in range(cols_size):
                     # Elegimos un bit aleatoriamente
                     bit_aleatorio = np.random.randint(0, valores_unicos[i])
                     # Construimos el bitarray
-                    
                     bits = bitarray(valores_unicos[i])
                     bits[bit_aleatorio] = 1
 
-                    diccionario = {datosTrain.columns[i]: bits}
-                    reglas.append(diccionario)
+                    reglas.append(bits)
 
-                individuo.append({n_regla: reglas})
+                individuo.append(reglas)
 
             poblacion.append(individuo)
                 
-        print(poblacion)
+        return poblacion
 
+    
+    def fitness(self, datosTrain, poblacion):
+        rows = datosTrain.shape[0]
+
+        fitness = []
+        for individuo in poblacion:
+            n_aciertos = 0
+            n_errores = 0
+            for regla in individuo:
+                for i in range(rows):
+                    instancia = datosTrain.iloc[i, :].values
+                    flag = True
+                    for atributo_idx, atributo in enumerate(regla):
+
+                        atributo_value = atributo.find(1)
+
+                        if atributo_idx == 0:
+                            atributo_value += 1
+
+                        if atributo_value != instancia[atributo_idx]:
+                            flag = False
+                            break
+
+                    if flag == True:
+                        n_aciertos += 1
+
+
+            fitness.append(n_aciertos / rows)
+
+        return fitness
+    
+    def entrenamiento(self, datosTrain, nominalAtributos, diccionario):
+        print(datosTrain)
+
+        poblacion = self.generar_poblacion(datosTrain)
+        fitness = self.fitness(datosTrain, poblacion)
+        print(fitness)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Datos import Datos
-from EstrategiaParticionado import ValidacionSimple
+from EstrategiaParticionado import ValidacionSimple, ValidacionCruzada
 from Clasificador import Clasificador
 from ClasificadorAlgoritmoGenetico import ClasificadorAlgoritmoGenetico
 
@@ -8,7 +8,7 @@ df = datos.datos
 nom_atributos = datos.nominalAtributos
 dic = datos.diccionarios
 
-simple = ValidacionSimple(1)
+simple = ValidacionCruzada(2)
 indices = simple.creaParticiones(df)
 
 indices_train = indices[0].indicesTrain
@@ -17,5 +17,5 @@ indices_test = indices[0].indicesTest
 datosTrain = datos.extraeDatos(indices_train)
 datosTest = datos.extraeDatos(indices_test)
 
-clf = ClasificadorAlgoritmoGenetico()
+clf = ClasificadorAlgoritmoGenetico(seed=42)
 clf.entrenamiento(datosTrain, nom_atributos, dic)
